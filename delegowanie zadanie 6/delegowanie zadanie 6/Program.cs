5fda7aa6e723674351369681ab1b68323be001c1
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-24 17:51:45 +0200

    Add files via upload

---
+++ delegowanie zadanie 6/delegowanie zadanie 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] numbers = { 2.5, 3.7, 1.2, 5.1, 4.9 };
+        int size = numbers.Length;
+        double average = CalculateRecursiveAverage(numbers, size);
+        Console.WriteLine("Średnia arytmetyczna: " + average);
+    }
+
+    static double CalculateRecursiveAverage(double[] numbers, int size)
+    {
+        if (size == 0)
+        {
+            return 0.0;
+        }
+
+        double lastNumber = numbers[size - 1];
+        double partialAverage = CalculateRecursiveAverage(numbers, size - 1);
+        double currentAverage = (partialAverage * (size - 1) + lastNumber) / size;
+
+        return currentAverage;
+    }
+}
