987a7d9995a071db68e5261c6d01368e59dd3ab2
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-24 17:49:47 +0200

    Add files via upload

---
+++ delegowanie zadanie 2/delegowanie zadanie 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x1 = 2.5;
+        double y1 = 3.8;
+        double x2 = -1.3;
+        double y2 = 7.2;
+
+        double length = CalculateSegmentLength(x1, y1, x2, y2);
+        Console.WriteLine("Długość odcinka: " + length);
+    }
+
+    static double CalculateSegmentLength(double x1, double y1, double x2, double y2)
+    {
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+
+        double length = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        return length;
+    }
+}
