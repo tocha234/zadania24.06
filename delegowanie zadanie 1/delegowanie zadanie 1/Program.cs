a8ae44631f192a4768e5c850b2f5ccd8829f2e0b
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-24 17:49:15 +0200

    Add files via upload

---
+++ delegowanie zadanie 1/delegowanie zadanie 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int elapsedTime = 332;
+        string formattedTime = FormatTime(elapsedTime);
+        Console.WriteLine(formattedTime);
+    }
+
+    static string FormatTime(int elapsedTime)
+    {
+        int hours = elapsedTime / 3600;
+        int minutes = (elapsedTime % 3600) / 60;
+        int seconds = (elapsedTime % 3600) % 60;
+
+        string formattedHours = hours.ToString("D2");
+        string formattedMinutes = minutes.ToString("D2");
+        string formattedSeconds = seconds.ToString("D2");
+
+        return $"{formattedHours}:{formattedMinutes}:{formattedSeconds}";
+    }
+}
