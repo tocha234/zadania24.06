afffa403ae4f969629d9d94cc0de3a414f0d46f9
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-24 17:51:12 +0200

    Add files via upload

---
+++ delegowanie zadanie 5/delegowanie zadanie 5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 20;
+        int primeDivisors = CountPrimeDivisors(n);
+        Console.WriteLine("Liczba dzielników pierwszych: " + primeDivisors);
+    }
+
+    static int CountPrimeDivisors(int n)
+    {
+        if (n <= 5)
+        {
+            return 0;
+        }
+
+        int primeDivisors = 0;
+
+        for (int i = 2; i <= n / 2; i++)
+        {
+            if (n % i == 0 && IsPrime(i))
+            {
+                primeDivisors++;
+            }
+        }
+
+        return primeDivisors;
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
