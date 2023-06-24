1b2eaefebe85648ed62ecc252115b50db36f3f4e
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-24 17:52:17 +0200

    Add files via upload

---
+++ tablice dwuwymiarowe zadanie 1/tablice dwuwymiarowe zadanie 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tablica = new int[10, 10];
+        int liczba = 1;
+
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            if (i % 2 == 0)
+            {
+                
+                for (int j = 0; j < tablica.GetLength(1); j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+            else
+            {
+                for (int j = tablica.GetLength(1) - 1; j >= 0; j--)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+        }
+
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            for (int j = 0; j < tablica.GetLength(1); j++)
+            {
+                Console.Write(tablica[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
