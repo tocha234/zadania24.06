a9ff101a4b553df663f24f2e2b2ed005353a1637
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-24 17:53:18 +0200

    Add files via upload

---
+++ tablice dwuwymiarowe zadanie 3/tablice dwuwymiarowe zadanie 3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rozmiar;
+        bool poprawnyRozmiar;
+
+        do
+        {
+            Console.Write("Podaj rozmiar tablicy: ");
+            string input = Console.ReadLine();
+
+            poprawnyRozmiar = int.TryParse(input, out rozmiar);
+
+            if (!poprawnyRozmiar || rozmiar <= 0)
+            {
+                Console.WriteLine("Podano niepoprawny rozmiar. Podaj dodatnią liczbę całkowitą.");
+            }
+
+        } while (!poprawnyRozmiar || rozmiar <= 0);
+
+        int[,] tablica = new int[rozmiar, rozmiar];
+        int liczba = 1;
+
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < tablica.GetLength(1); j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+            else
+            {
+                for (int j = tablica.GetLength(1) - 1; j >= 0; j--)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+        }
+
+        for (int i = 0; i < tablica.GetLength(0); i++)
+        {
+            for (int j = 0; j < tablica.GetLength(1); j++)
+            {
+                Console.Write(tablica[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
