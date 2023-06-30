e28adb23aa595b1d89b1e664b314d97e43cb6b8a
tocha234 <129594026+tocha234@users.noreply.github.com>
2023-06-30 16:36:34 +0200

    Add files via upload

---
+++ typydanych zadanie 1/typydanych zadanie 1/Program.cs
@@ -0,0 +1,61 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DniTygodnia dzien = DniTygodnia.Sroda;
+        Console.WriteLine("Dzisiejszy dzień to: " + dzien);
+
+        EtapyPrania etap = EtapyPrania.Pranie;
+        Console.WriteLine("Aktualny etap prania: " + etap);
+
+        Posilki posilek = Posilki.Obiad;
+        Console.WriteLine("Czas na: " + posilek);
+
+        BierkiSzachowe bierka = BierkiSzachowe.Wieza;
+        Console.WriteLine("Wybrana bierka to: " + bierka);
+    }
+    public enum DniTygodnia
+    {
+        Poniedzialek,
+        Wtorek,
+        Sroda,
+        Czwartek,
+        Piatek,
+        Sobota,
+        Niedziela
+    }
+
+    public enum EtapyPrania
+    {
+        WkladaniePomocnicze,
+        Sortowanie,
+        WyborProgramu,
+        DodawanieDetergentu,
+        Pranie,
+        Plyniecie,
+        Piorowanie,
+        PlynDoPiora,
+        Suszenie,
+        Skladanie
+    }
+
+    public enum Posilki
+    {
+        Sniadanie,
+        DrugieSniadanie,
+        Obiad,
+        Podwieczorek,
+        Kolacja
+    }
+
+    public enum BierkiSzachowe
+    {
+        Pion,
+        Wieza,
+        Skoczek,
+        Goniec,
+        Hetman,
+        Krol
+    }
+
+}
